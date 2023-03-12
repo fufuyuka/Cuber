4860ad8a4f7bcc42e0e75887d8e9ff9970b5ba80
fufuyuka <121597200+fufuyuka@users.noreply.github.com>
2023-03-12 13:09:12 +0000

    contact-form

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -9,5 +9,17 @@ class Public::HomesController < ApplicationController
   end
   
   def create
+    @contact = Contact.new(contact_params)
+     if @contact.save
+      redirect_to contacts_completion_path
+    else
+      render 'new'
+    end
+  end
+  
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name,:email,:contact_detail)
   end
 end
