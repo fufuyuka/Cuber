5fd6a2318fa587d6486df2c74d2d36f53e80b3cf
fufuyuka <121597200+fufuyuka@users.noreply.github.com>
2023-03-24 05:26:58 +0000

    [fix]profile-image

--- app/controllers/public/user/profiles_controller.rb
+++ app/controllers/public/user/profiles_controller.rb
@@ -1,7 +1,7 @@
 class Public::User::ProfilesController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_guest_user, only: [:edit]
-  before_action :current_user!, except: [:show]
+  before_action :current_user, except: [:show]
   
   def show
     @user = User.find(params[:id])
