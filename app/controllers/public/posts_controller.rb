50f05f1e26aff8dff8632a3bc408e0fd94082442
fufuyuka <121597200+fufuyuka@users.noreply.github.com>
2023-04-29 10:30:43 +0000

    [add]post-errer-messages

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -17,7 +17,6 @@ class Public::PostsController < ApplicationController
     # 保存に失敗したら
     unless @new_post.save
       @posts = Post.order(created_at: :desc).page(params[:page])
-      flash[:notice] = "文字数を確認してください"
       render 'index'
       return
     end
